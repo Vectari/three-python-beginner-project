b9102133b6eb113e670188fda15b4a09fc0c526a
Vectari <mateusz.majer996@gmail.com>
2024-08-20 00:18:34 +0200

    feat: finish project

--- project#3_timed_math_challenge.py
+++ project#3_timed_math_challenge.py
@@ -1,9 +1,10 @@
 import random
+import time
 
 OPERATORS = ["+", "-", "*"]
 MIN_OPERAND = 1
-MAX_OPERAND = 30
-TOTAL_PROBLEMS = 10
+MAX_OPERAND = 5
+TOTAL_PROBLEMS = 5
 
 def generate_problem():
   left = random.randint(MIN_OPERAND, MAX_OPERAND)
@@ -14,9 +15,22 @@ def generate_problem():
   answer = eval(expr)
   return (expr, answer)
 
+wrong = 0
+input("Press enter to start!")
+print("----------------------")
+
+start_time = time.time()
+
 for i in range(TOTAL_PROBLEMS):
   expr, answer = generate_problem()
   while True:
     guess = input("Problem #" + str(i + 1) + ": " + expr + " = ")
     if guess == str(answer):
       break
+    wrong += 1
+
+end_time = time.time()
+total_time = round(end_time - start_time, 2)
+
+print("----------------------")
+print("Nice work! You finished in", total_time, "seconds!")
